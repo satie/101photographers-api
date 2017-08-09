101822cd1ff1712f1c617218445d780261f1b485
satie <satiesharma@gmail.com>
2017-08-09 18:21:06 -0400

    Temp commit; added input validation

--- contact/handler.js
+++ contact/handler.js
@@ -1,21 +1,30 @@
 'use strict';
 
 const sesForwarder = require('aws-lambda-ses-forwarder');
+const validator = require('validator');
 
 module.exports.sendContactEmail = (event, context, callback) => {
-  if (event.httpMethod === "POST" && && event.body.name && event.body.email && event.body.message) {
-    const response = {
-      statusCode: 200,
-      name: event.name,
-      email: event.email,
-      message: event.message
-    };
-  } else {
-    const response = {
-      statusCode: 500,
-      errorMessage: 'Invalid request'
-    };
+  if (event.httpMethod === "POST" && event.body) {
+    let contact = JSON.parse(event.body);
+    if (validator.isEmail(contact.email) && !validator.isEmpty(contact.name)) {
+      let overrides = {
+        config: {
+          fromEmail: "contact@101photographers.in",
+          forwardMapping: {
+            "@101photographers.in": [
+              "satiesharma@gmail.com"
+            ]
+          }
+        }
+      };
+      sesForwarder.handler(event, context, callback, overrides);
+    }
+    // return callback(null, {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //     message: 'Hello',
+    //     object: contact
+    //   }),
+    // });
   }
-
-  callback(null, response);
 };
