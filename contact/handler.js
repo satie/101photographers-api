98571dfae55f185cc8f2518884dca033a3b55e70
satie <satiesharma@gmail.com>
2017-08-13 12:06:50 -0400

    Send email with SES

--- contact/handler.js
+++ contact/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const sesForwarder = require('aws-lambda-ses-forwarder');
+// const sesForwarder = require('aws-lambda-ses-forwarder');
+const aws = require('aws-sdk');
 const validator = require('validator');
 
 module.exports.sendContactEmail = (event, context, callback) => {
@@ -17,7 +18,38 @@ module.exports.sendContactEmail = (event, context, callback) => {
           }
         }
       };
-      sesForwarder.handler(event, context, callback, overrides);
+      let msg = contact.message ? "New contact from " + contact.email + " through 101photographers - " + contact.message : "New contact from " + contact.email + " through 101photographers";
+      let email = {
+        Source: "101photographers Contact Form <contact@101photographers.in>",
+        Destination: {
+          ToAddresses: [ "satiesharma@gmail.com" ]
+        },
+        Message: {
+          Subject: {
+            Data: "New contact from 101photographers"
+          },
+          Body: {
+            Text: {
+              Data: msg
+            }
+          }
+        },
+        ReplyToAddresses: [contact.email]
+      };
+      const ses = new aws.SES();
+      let sesPromise = ses.sendEmail(email).promise();
+      sesPromise.then((data) => {
+        console.log(data);
+        return callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({
+            messageId: data
+          })
+        });
+      }).catch((error) => {
+        callback(error, null);
+      });
+      // sesForwarder.handler(event, context, callback, overrides);
     }
     // return callback(null, {
     //   statusCode: 200,
